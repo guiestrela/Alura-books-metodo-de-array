d778ac8ebec8f1733b15491ceede0a0f7324c5fa
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-09-19 22:25:27 -0300

    finalizaçao

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -7,7 +7,8 @@ function filtrarLivros() {
     let livrosFiltrados = categoria == 'disponivel' ? filtarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
     if (categoria == 'disponivel') {
-        exibirValorTotalDoslivrosDispomiveisNaTela()
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDoslivrosDispomiveisNaTela(valorTotal)
     }
 }
 
@@ -19,10 +20,10 @@ function filtarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0)
 }
 
-function exibirValorTotalDoslivrosDispomiveisNaTela() {
+function exibirValorTotalDoslivrosDispomiveisNaTela(valorTotal) {
     elementoComValorDeLivrosDisponiveis.innerHTML = `
         <div class="livros__disponiveis">
-            <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+            <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
         </div>
     `
 }
